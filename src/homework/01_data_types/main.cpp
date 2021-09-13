b3d7bb264532fa48786e9c747d97bade64986e61
TienPham162 <tien.pham3@g.austincc.edu>
2021-09-13 17:23:37 -0500

    hw 1 w test cases

--- src/homework/01_data_types/main.cpp
+++ src/homework/01_data_types/main.cpp
@@ -1,15 +1,27 @@
 ﻿//write include statements
-
+#include <iostream>
+#include "data_types.h"
 
 //write namespace using statement for cout
-
+using namespace std;
 
 /*
 Call multiply_numbers with 10 and 10 parameter values and display function result
 */
-int main()
+int main() 
 {
-	
+	int num;
 
-	return 0;
+cout<<"Enter a number: ";
+cin>>num;
+
+int result = multiply_numbers(num);
+cout<<"Result = "<<result<<endl;
+
+int num1 = 4;
+
+    result = multiply_numbers(num1);
+	cout<<"Result = "<<result<<endl;
+
+return 0;
 }
