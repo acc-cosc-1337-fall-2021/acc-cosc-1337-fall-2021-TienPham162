bcdf5ffad14311e657bb12d916533c9f07e5399b
TienPham162 <tien.pham3@g.austincc.edu>
2021-10-01 22:19:38 -0500

    hw-04-repetition

--- src/homework/04_repetition/dna.cpp
+++ src/homework/04_repetition/dna.cpp
@@ -1,3 +1,38 @@
 //add include statements
+#include "dna.h"
 
 //add function(s) code here
+
+int factorial(int num)
+{
+    int sum;
+    sum = 1;
+    while (num > 0)
+    {
+        sum = sum * num;
+        num = num - 1;
+        
+    }
+    return sum;
+
+}
+
+int gcd(int num1, int num2)
+{
+    while(num1 != num2)
+    {
+        if (num1 > num2)
+        { 
+            int temp;
+            temp = num1;
+            num1 = num2;
+            num2 = temp;
+        }
+        num2 = num2 - num1;
+       
+    }
+    
+     return num1;
+    
+
+}
--- src/homework/04_repetition/main.cpp
+++ src/homework/04_repetition/main.cpp
@@ -1,7 +1,9 @@
 //write include statements
+#include "dna.h"
+#include <iostream>
 
 //write using statements
-
+using namespace std;
 
 /*
 Write code that prompts user to enter 1 for Get GC Content, 
@@ -12,5 +14,65 @@ user enters a y or Y.
 */
 int main() 
 {
+	char options;
+	MENU: do
+	{ 
+		cout << "Menu\n\n";
+		cout << "1-Factorial" << endl;
+		cout << "2-Greatest Common Divisor" << endl;
+		cout << "3-Exit\n";
+		cout << "Enter your option number: ";
+
+		cin >> options;
+
+	}
+	while (options != '1' && options != '2' && options != '3');
+
+	if (options == '1')
+	{
+		int number, sum;
+		cout << "Enter a number: ";
+		cin >> number;
+		sum = factorial(number);
+
+		cout << "The factorial sum is: " << sum << endl;
+
+	}
+	
+	else if (options == '2')
+	{
+		int num1, num2, result;
+		cout << "Enter first number: ";
+		cin >> num1;
+		cout << "Enter second number: ";
+		cin >> num2;
+		result = gcd(num1, num2);
+
+		cout << "The gcd number is: " << result << endl;
+
+	}
+	
+	else 
+	{
+	
+		char confirm;
+		do
+		{
+			cout << "Are you sure you want to exit? (y/n) ";
+			cin >> confirm;
+		}
+		while(confirm != 'y' && confirm != 'Y' && confirm != 'n' && confirm != 'N' );
+		
+		if (confirm == 'y' || confirm =='Y')
+		{
+			return 0;
+		}
+		else
+		{
+			goto MENU;
+		}
+	
+	}
 	return 0;
+
 }
